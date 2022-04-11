c14a4e36520e33fad89438b101ad14b1c938207e
Ankur Sheel <ankur.sheel@gmail.com>
2022-04-11 22:41:35 +1200

    Add DI and logging

--- FeatureFlagHelper/Program.cs
+++ FeatureFlagHelper/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceProvider = new ServiceCollection()
+    .AddLogging()
+    .BuildServiceProvider();
+
 Console.WriteLine("Hello, World!");

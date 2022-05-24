6da70e785ab84d1d0f18726adc2b377e6f11ca38
Ankur Sheel <ankur.sheel@gmail.com>
2022-05-25 09:11:55 +1200

    throw exception if settings are not set

--- FeatureFlagHelperConsole/Settings.cs
+++ FeatureFlagHelperConsole/Settings.cs
@@ -4,6 +4,11 @@ public record Settings
 {
     public Settings(string enumPath, string jsonFilesDirectory, string ownedBy)
     {
+        if (string.IsNullOrWhiteSpace(enumPath) || string.IsNullOrWhiteSpace(jsonFilesDirectory) || string.IsNullOrWhiteSpace(ownedBy))
+        {
+            throw new NullReferenceException("Properties not set in appSettings.json");
+        }
+        
         EnumPath = enumPath;
         OwnedBy = ownedBy;
         JsonFilePaths = Directory.GetFiles(jsonFilesDirectory, "*.*.json");
